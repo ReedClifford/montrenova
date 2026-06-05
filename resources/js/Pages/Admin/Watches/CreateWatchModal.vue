5e6c326eb518e43a97e454d4ec616507ede0bc50
hakubaaaa@gmail.com <hakubaaaa@gmail.com>
2026-06-05 19:43:54 +0800

    fixed

--- resources/js/Pages/Admin/Watches/CreateWatchModal.vue
+++ resources/js/Pages/Admin/Watches/CreateWatchModal.vue
@@ -43,7 +43,7 @@ const form = useForm({
     selling_price: 0,
     discounted_price: "",
 
-    status: "is_visible",
+    status: "available",
     is_featured: false,
     is_visible: true,
     display_price: true,
